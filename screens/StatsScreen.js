64e6d94998290fb8aaeb38b61c5cca422665a3de
gul371 <121416gul@gmail.com>
2025-09-06 14:20:25 -0700

    Create StatsScreen.js

---
+++ screens/StatsScreen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function StatsScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Stats Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ffffff',
+  },
+  text: {
+    fontSize: 18,
+  },
+});
